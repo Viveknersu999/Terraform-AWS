6606a37638552928720f00aa664cfba74186f05a
VivekNersu999 <nersu.vivek9@gmail.com>
2023-05-11 19:12:50 +0530

    adding main file

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "ec2_instance" {
+    ami = "ami-076b75d50e423acfe"
+    instance_type = "t2.micro"
+    key_name = "ec2-jenkins.pem"
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+    private_key = file("keypair/ec2-jenkins.pem")
+} 
+  tags = {
+    Name = "webapp"
+  }
+}
